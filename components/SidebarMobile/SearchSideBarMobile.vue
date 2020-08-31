d7bc496ebcf3a72ecd90eafa9a5ac96f0ac456f3
nguyentam2206 <nguyenthientam.2206@gmail.com>
2020-08-31 21:30:13 +0700

    fix transition

--- components/SidebarMobile/SearchSideBarMobile.vue
+++ components/SidebarMobile/SearchSideBarMobile.vue
@@ -1,5 +1,4 @@
 <template>
-  <transition name="slide">
     <div class="search-sidebar">
       <div class="search-sidebar__icon relative">
         <i class="relative fas fa-times" @click="$emit('closeSideBar')"></i>
@@ -9,7 +8,6 @@
         <input type="text" ref="input" class="search-sidebar__search-input" />
       </div>
     </div>
-  </transition>
 </template>
 
 <script>
@@ -45,20 +43,20 @@ export default {
   background-image: url("../../assets/imgs/sidebar-background.jpg");
   background-size: cover;
   background-position: center top;
-  animation: right 0.3s ease-in-out;
+  /* animation: right 0.3s ease-in-out;
   -webkit-transition: all 0.5s cubic-bezier(0.79, 0.14, 0.15, 0.86);
-  transition: all 0.5s cubic-bezier(0.79, 0.14, 0.15, 0.86);
+  transition: all 0.5s cubic-bezier(0.79, 0.14, 0.15, 0.86); */
   /* transform: translate3d(100%, 0, 0); */
   /* -webkit-transform: translate3d(100%, 0, 0); */
 }
-@keyframes right {
+/* @keyframes right {
   from {
     transform: translate3d(100%, 0, 0);
   }
   to {
     transform: translate3d(0, 0, 0);
   }
-}
+} */
 
 /* before */
 
--- pages/index.vue
+++ pages/index.vue
@@ -9,11 +9,14 @@
       <!-- nav mobile -->
       <NaviMobile @openSideBar="showSideBar = true" @openSearchSideBar="showSearchSideBar = true" />
       <!-- sidebar menu -->
-      <SideBarMobile v-if="showSideBar" @closeSideBar="showSideBar = false" />
+      <transition name="slide-left">
+        <SideBarMobile v-if="showSideBar" @closeSideBar="showSideBar = false" />
+      </transition>
       <!-- sidebar search -->
-      <SearchSideBarMobile v-if="showSearchSideBar" @closeSideBar="showSearchSideBar = false" />
+      <transition name="slide-right">
+        <SearchSideBarMobile v-if="showSearchSideBar" @closeSideBar="showSearchSideBar = false" />
+      </transition>
     </div>
-
     <BlockA class="mt-10" />
     <SocialBlock class="mt-10" />
     <BlockB class="mt-10" />
@@ -60,5 +63,29 @@ export default {
 </script>
 
 <style>
-/* test code */
+.slide-left-enter-active {
+  transition: all 0.5s cubic-bezier(0.79, 0.14, 0.15, 0.86);
+  transform: translate3d(0, 0, 0);
+}
+.slide-left-leave-active {
+  transition: all 0.5s cubic-bezier(0.79, 0.14, 0.15, 0.86);
+  transform: translate3d(0, 0, 0);
+}
+.slide-left-enter,
+.slide-left-leave-to {
+  transform: translate3d(-100%, 0, 0);
+}
+
+.slide-right-enter-active {
+  transition: all 0.5s cubic-bezier(0.79, 0.14, 0.15, 0.86);
+  transform: translate3d(0, 0, 0);
+}
+.slide-right-leave-active {
+  transition: all 0.5s cubic-bezier(0.79, 0.14, 0.15, 0.86);
+  transform: translate3d(0, 0, 0);
+}
+.slide-right-enter,
+.slide-right-leave-to {
+  transform: translate3d(100%, 0, 0);
+}
 </style>
